2a029472e99b0aeaee7b54454ec851461a841d2a
Leonardo Oliveira Tieppo <leonardo.tieppo2@hotmail.com>
2024-06-20 12:55:32 -0300

    alterar dockerfile CG

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# Iniciar o backend
 cd /app/backend
-node server.js
+node server.js &
+
+# Iniciar o frontend (supondo que o frontend seja iniciado com serve)
+cd /app/modernchef
+npm start
